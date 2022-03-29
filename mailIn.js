d5752fbc6213baa2fd47736d9e247ccbc17cb92e
kays0n <haison.33@gmail.com>
2022-03-29 10:07:51 +0700

    add From getCode screen

--- mailIn.js
+++ mailIn.js
@@ -314,12 +314,12 @@ app.get('/api/verify/email/:email/:timeout', async (req, res) => {
     let result = {};
     if (value){
         data = JSON.parse(value);
-        let result = {"success":true,"error":"","data":data.code,"content":data.content};
+        let result = {"success":true,"error":"","data":data.code,"content":data.content, "from":data.from};
         console.log(result);
         res.send(result);
     }
     else {
-        res.send({"success":false,"error":"No data found","data":null,"content":""});
+        res.send({"success":false,"error":"No data found","data":null,"content":"", "from":""});
     }
 })
 
