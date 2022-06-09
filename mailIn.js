40ac0f0261c260588ffe9fa11dc829f83e76121d
kays0n <haison.33@gmail.com>
2022-06-09 14:35:31 +0700

    update port

--- mailIn.js
+++ mailIn.js
@@ -22,7 +22,7 @@ client.on("error", function(error) {
 const express = require('express')
  var path = require('path');
 const app = express();
-const port = 4000;
+const port = 80;
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
